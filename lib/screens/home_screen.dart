1865a4918d0ab0ba800dbb0635427b932834127a
psgwei <psgwei@protonmail.com>
2024-01-09 14:55:58 +0800

    bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/models/user.dart';
-import 'package:instagram_clone/providers/user_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:instagram_clone/utilities/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,30 +9,80 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int page = 0;
+  PageController pageController = PageController();
+
   @override
-  void initState() {
-    super.initState();
-    getUser();
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
   }
 
-  void getUser() async {
-    //retrieve an instance of UserProvider from the nearest ancestor Provider in the widget tree
-    // listen:false -> get the value one time
-    UserProvider userProvider = Provider.of<UserProvider>(context);
-    await userProvider.refreshUser();
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
   }
 
   @override
   Widget build(BuildContext context) {
-    User? user = Provider.of<UserProvider>(context).getUser;
-    if (user != null) {
-      return Center(
-        child: Text(user.username),
-      );
-    } else {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+    return Scaffold(
+      body: PageView(
+        children: [
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4'),
+          Text('5'),
+        ],
+        controller: pageController,
+        onPageChanged: (value) {
+          setState(() {
+            page = value;
+          });
+        },
+        physics:
+            const NeverScrollableScrollPhysics(), //disable swipe left and right to change page
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: page == 0 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: mobileBackgroundColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: page == 1 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: page == 2 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: page == 3 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: page == 4 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+          ),
+        ],
+        onTap: navigationTapped,
+      ),
+    );
   }
 }
